3311f9e04eb771e125d39492842cfc1ff96fdecd
Fruitloopins <49851457+Fruitloopins@users.noreply.github.com>
2024-03-17 21:13:16 +1100

    Return default dynmap colour if none is set

--- src/main/java/net/earthmc/emcapi/manager/NationMetadataManager.java
+++ src/main/java/net/earthmc/emcapi/manager/NationMetadataManager.java
@@ -4,19 +4,20 @@ import com.palmergames.bukkit.towny.object.Nation;
 import com.palmergames.bukkit.towny.object.metadata.StringDataField;
 
 public class NationMetadataManager {
+    private static final String DEFAULT_DYNMAP_COLOUR = "3FB4FF";
 
     public static String getDynmapColour(Nation nation) {
         StringDataField sdf = (StringDataField) nation.getMetadata("dynmapColour");
-        if (sdf == null) return null;
+        if (sdf == null) return DEFAULT_DYNMAP_COLOUR;
 
-        return sdf.getValue();
+        return sdf.getValue().toUpperCase();
     }
 
     public static String getDynmapOutline(Nation nation) {
         StringDataField sdf = (StringDataField) nation.getMetadata("dynmapOutline");
-        if (sdf == null) return null;
+        if (sdf == null) return DEFAULT_DYNMAP_COLOUR;
 
-        return sdf.getValue();
+        return sdf.getValue().toUpperCase();
     }
 
     public static String getWikiURL(Nation nation) {
