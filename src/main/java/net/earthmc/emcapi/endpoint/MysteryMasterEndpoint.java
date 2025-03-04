b27e00c7725d4ada92ee8970d7acea3b00f1636e
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2025-03-04 21:25:26 +0100

    Use mystery master api artifact

--- src/main/java/net/earthmc/emcapi/endpoint/MysteryMasterEndpoint.java
+++ src/main/java/net/earthmc/emcapi/endpoint/MysteryMasterEndpoint.java
@@ -6,41 +6,26 @@ import com.google.gson.JsonObject;
 import io.javalin.http.ServiceUnavailableResponse;
 import net.earthmc.emcapi.EMCAPI;
 import net.earthmc.emcapi.object.endpoint.GetEndpoint;
-import net.earthmc.emcapi.service.mysterymaster.MysteryMasterService;
-import net.earthmc.emcapi.service.mysterymaster.MysteryPlayer;
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.Plugin;
+import net.earthmc.mysterymaster.api.MysteryMasterAPI;
+import net.earthmc.mysterymaster.api.MysteryMasterProvider;
+import net.earthmc.mysterymaster.api.MysteryPlayer;
 
 import java.util.List;
-import java.util.ServiceConfigurationError;
-import java.util.ServiceLoader;
 
 public class MysteryMasterEndpoint extends GetEndpoint {
 
-    private final MysteryMasterService service;
+    private final MysteryMasterAPI api = MysteryMasterProvider.api();
+    private final ServiceUnavailableResponse UNAVAILABLE = new ServiceUnavailableResponse("Mystery Master details are not available currently");
 
     public MysteryMasterEndpoint() {
-        final Plugin mm = Bukkit.getPluginManager().getPlugin("MysteryMaster");
-        if (mm == null) {
+        if (api == null) {
             EMCAPI.instance.getLogger().warning("Not loading mystery master endpoint due to the plugin not being present");
-            service = null;
-            return;
         }
-
-        MysteryMasterService loaded = null;
-
-        try {
-            loaded = ServiceLoader.load(MysteryMasterService.class, mm.getClass().getClassLoader()).findFirst().orElseThrow(() -> new IllegalStateException("Could not find MysteryMasterService"));
-        } catch (ServiceConfigurationError | IllegalArgumentException | IllegalStateException e) {
-            EMCAPI.instance.getSLF4JLogger().warn("Failed to load mystery master service", e);
-        }
-
-        service = loaded;
     }
 
     @Override
     public String lookup() {
-        if (service == null) throw new ServiceUnavailableResponse("Mystery Master details are not available currently");
+        if (api == null) throw UNAVAILABLE;
         return getJsonElement().toString();
     }
 
@@ -48,7 +33,7 @@ public class MysteryMasterEndpoint extends GetEndpoint {
     public JsonElement getJsonElement() {
         JsonArray jsonArray = new JsonArray();
 
-        List<? extends MysteryPlayer> players = service.getCurrentTopPlayers();
+        List<MysteryPlayer> players = api.getCurrentTopPlayers();
         for (int i = 0; i < Math.min(50, players.size()); i++) {
             MysteryPlayer player = players.get(i);
             JsonObject jsonObject = new JsonObject();
